9883e7f6bc2073772b458e97f817903d63d03a8c
playatanu <playatanu@gmail.com>
2023-03-02 16:12:33 +0530

    up

--- app.js
+++ app.js
@@ -17,14 +17,23 @@ mongoose.connect(MONGO_URL, () => console.log("Connected to MongoDB"));
 
 app.use(cors());
 
+const IMAGE_SERVER_URL = process.env.IMAGE_SERVER_URL;
+const ML_SERVER_URL = process.env.ML_SERVER_URL;
+const EMAIL_SERVER_URL = process.env.EMAIL_SERVER_URL;
+const USER_SERVER_URL = process.env.USER_SERVER_URL;
+const PROPERTY_SERVER_URL = process.env.PROPERTY_SERVER_URL;
+const CHART_SERVER_URL = process.env.CHART_SERVER_URL;
+const WS_SERVER = process.env.WS_SERVER;
+
 app.use("/auth", require("./routes/auth"));
-app.use("/cloud", proxy("http://127.0.0.1:8081/image/"));
-app.use("/image", proxy("http://127.0.0.1:8081"));
-app.use("/ml", authenticate, proxy("http://127.0.0.1:8082"));
-app.use("/mail", authenticate, proxy("http://127.0.0.1:8083"));
-app.use("/user", authenticate, proxy("http://127.0.0.1:8084"));
-app.use("/property", authenticate, proxy("http://127.0.0.1:8085"));
-app.use("/chat", authenticate, proxy("http://127.0.0.1:8086"));
+app.use("/cloud", proxy(IMAGE_SERVER_URL + "/image/"));
+app.use("/image", proxy(IMAGE_SERVER_URL));
+app.use("/ml", authenticate, proxy(ML_SERVER_URL));
+app.use("/mail", authenticate, proxy(EMAIL_SERVER_URL));
+app.use("/user", authenticate, proxy(USER_SERVER_URL));
+app.use("/property", authenticate, proxy(PROPERTY_SERVER_URL));
+app.use("/chat", authenticate, proxy(CHART_SERVER_URL));
+app.use("/ws", proxy(WS_SERVER));
 
 app.listen(PORT, () => console.warn(`Running on http://127.0.0.1:${PORT}`));
 
