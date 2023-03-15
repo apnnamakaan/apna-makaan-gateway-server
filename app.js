585dbd8ef8c0c92e65a398ea999b6e784e17a220
playatanu <playatanu@gmail.com>
2023-03-15 19:51:48 +0530

    update 15-03-23

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.set("strictQuery", false);
-mongoose.connect(MONGO_URL, () => console.log("Connected to MongoDB"));
+mongoose.connect(MONGO_URL);
 
 app.use(cors());
 
@@ -22,9 +22,10 @@ const ML_SERVER_URL = process.env.ML_SERVER_URL;
 const EMAIL_SERVER_URL = process.env.EMAIL_SERVER_URL;
 const USER_SERVER_URL = process.env.USER_SERVER_URL;
 const PROPERTY_SERVER_URL = process.env.PROPERTY_SERVER_URL;
-const CHART_SERVER_URL = process.env.CHART_SERVER_URL;
+const CHAT_SERVER_URL = process.env.CHAT_SERVER_URL;
 const WS_SERVER = process.env.WS_SERVER;
 
+app.get('/', (req, res) => res.send({ status: 'true', author: "Atanu Debnath", about: 'gatway server' }));
 app.use("/auth", require("./routes/auth"));
 app.use("/cloud", proxy(IMAGE_SERVER_URL + "/image/"));
 app.use("/image", proxy(IMAGE_SERVER_URL));
@@ -32,9 +33,9 @@ app.use("/ml", authenticate, proxy(ML_SERVER_URL));
 app.use("/mail", authenticate, proxy(EMAIL_SERVER_URL));
 app.use("/user", authenticate, proxy(USER_SERVER_URL));
 app.use("/property", authenticate, proxy(PROPERTY_SERVER_URL));
-app.use("/chat", authenticate, proxy(CHART_SERVER_URL));
+app.use("/chat", authenticate, proxy(CHAT_SERVER_URL));
 app.use("/ws", proxy(WS_SERVER));
 
-app.listen(PORT, () => console.warn(`Running on http://127.0.0.1:${PORT}`));
+app.listen(PORT, '127.0.0.1', () => console.warn(`Running on PORT ${PORT}`));
 
 
